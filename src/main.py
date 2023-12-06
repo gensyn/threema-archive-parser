c7195609f6d279c09601ee616507da069e4ca371
Gensyn <gensyn@posteo.de>
2023-12-06 12:15:20 +0100

    CSS ergänzt und anzeige generell aufgehübscht

--- src/main.py
+++ src/main.py
@@ -5,11 +5,14 @@ from os.path import isdir, isfile, join, basename
 from pathlib import Path
 
 FILE_REGEX = [
-    "<([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}\.([a-z0-9]{3,4}))>",
-    "<([a-z0-9]{16}-[a-z]*-[0-9]{8}-[0-9]{6,10}\.([a-z0-9]{3,4}))>"
+    "(&lt;([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}\.([a-z0-9]{3,4}))&gt;)",
+#    "(&lt;([a-z0-9]{16}-[a-z]*-[0-9]{8}-[0-9]{6,10}\.([a-z0-9]{3,4}))&gt;)",
+    "(&lt;([a-z0-9]{16}-.*.([a-z0-9]{3,4}))&gt;)"
 ]
 
 URL_REGEX = "(https?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)"
+
+MESSAGE_REGEX = "\[(\d{1,2}\.\d{1,2}\.\d{4}), (\d{1,2}:\d{2})\] (.+?): (.*)"
 
 parser = argparse.ArgumentParser("Threema HTML Generator")
 parser.add_argument("folder", help="The folder containing the Threema Chats.")
@@ -18,7 +21,36 @@ args = parser.parse_args()
 folder = args.folder
 
 if not isdir(folder):
-    raise ValueError("Given value for \"folder\" is not an acutally folder on this machine.")
+    raise ValueError("Given value for \"folder\" is not an acutal folder on this machine.")
+
+
+def parse_message(path, message):
+    message = message.replace("<", "&lt;").replace(">", "&gt;")
+
+    for regex in FILE_REGEX:
+        matches = re.findall(regex, message)
+
+        for match in matches:
+            if not isfile(join(path, match[1])):
+                message = message.replace(f"{match[0]}", f"<a href=\"{match[1]}\">DATEI FEHLT</a>")
+            elif match[2] in ["jpg", "jpeg", "png"]:
+                message = message.replace(f"{match[0]}", f"<br/><a href=\"{match[1]}\"><img src=\"{match[1]}\" width=300 /></a><br />")
+            elif match[2] in ["mp4"]:
+                message = message.replace(f"{match[0]}", f"<br /><a href=\"{match[1]}\"><video controls><source src=\"{match[1]}\" /></video></a><br />")
+            else:
+                message = message.replace(f"{match[0]}", f"<a href=\"{match[1]}\">{match[1]}</a>")
+
+    matches = list(set(re.findall(URL_REGEX, message)))
+
+    for match in matches:
+        message = message.replace(match, f"<a href=\"{match}\">{match}</a>")
+
+    match = re.search(MESSAGE_REGEX, message)
+
+    if match:
+        return match.group(1), match.group(2), match.group(3), match.group(4)
+
+    return None, None, None, message
 
 
 def create_html(path):
@@ -30,29 +62,50 @@ def create_html(path):
     name = basename(path)
 
     with open(messages_path, "r") as f:
-        messages = '\n'.join([f"<p>{message}</p>" for message in f.readlines()])
+        messages = f.readlines()
+
+    content = ""
+    previous_date = None
+    previous_time = None
+    previous_align = None
+
+    for message in messages:
+        date, time, author, message_content = parse_message(path, message)
+
+        if not date:
+            # this message is just a new line of the previous message
+            content += f"<br />{message_content}"
+            continue
+
+        align = "right" if author == "Ich" else "left"
+
+        if previous_time:
+            # this is a new message - write time of previous message
+            content += f"<br /><span class=\"time time-{previous_align}\">{previous_time}</span>"
+
+        if previous_date:
+            # we are not processing the first element
+            content += "</div></div>"
+
+        if date != previous_date:
+            content += f"<div class=\"date\">{date}</div>"
+
+        content += f'<div class=\"wrapper wrapper-{align}\"><div class=\"message {align}\">'
+
+        if author != "Ich":
+            content += f'<span class=\"author">{author}</span><br />'
+
+        content += f"{message_content}"
+
+        previous_date = date
+        previous_time = time
+        previous_align = align
+
+    content += "</div></div>"
 
     template = Path("src/index_template.html").read_text()
 
-    html = template.replace("$title", name).replace("$content", f"<div>{messages}</div>")
-
-    for regex in FILE_REGEX:
-        matches = re.findall(regex, html)
-
-        for match in matches:
-            if not isfile(join(path, match[0])):
-                html = html.replace(f"<{match[0]}>", f"<a href=\"{match[0]}\">DATEI FEHLT</a>")
-            elif match[1] in ["jpg", "jpeg", "png"]:
-                html = html.replace(f"<{match[0]}>", f"</p><p><a href=\"{match[0]}\"><img src=\"{match[0]}\" width=300 /></a></p><p>")
-            elif match[1] in ["mp4"]:
-                html = html.replace(f"<{match[0]}>", f"</p><p><a href=\"{match[0]}\"><video controls><source src=\"{match[0]}\" /></video></a></p><p>")
-            else:
-                html = html.replace(f"<{match[0]}>", f"<a href=\"{match[0]}\">{match[0]}</a>")
-
-    matches = list(set(re.findall(URL_REGEX, html)))
-
-    for match in matches:
-        html = html.replace(match, f"<a href=\"{match}\">{match}</a>")
+    html = template.replace("$title", name).replace("$content", f"{content}")
 
     with open(join(path, "index.html"), "w") as f:
         f.write(html)
