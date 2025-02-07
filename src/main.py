bf16ae2e459c656e6bf303ea147a6d733212f321
Gensyn <gensyn@posteo.de>
2025-02-07 19:08:58 +0100

    Switched from HTML to SQLite

--- src/main.py
+++ src/main.py
@@ -2,269 +2,148 @@ import argparse
 import os
 import re
 import shutil
-from os import listdir
+import sqlite3
+from os import listdir, remove, mknod
 from os.path import isdir, isfile, join, basename
-from pathlib import Path
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(join(dname, ".."))
 
-FILE_REGEX = r"(&lt;((?:[a-f0-9]{8}|[a-f0-9]{16})-.*?\.([a-z0-9]{3,4}))&gt;)"
+REGEX_SPLIT_BY_NAME = (r"\[(\d{1,2})\.(\d{1,2})\.(\d{4}),\s(\d{1,2}):(\d{2})\]\s(.+?):\s(.*?)("
+                       r"?=\n\[\d{1,2}\.\d{1,2}\.\d{4},\s\d{1,2}:\d{2}\]|$)")
+
+FILE_REGEX = (r"(Bild|Video|Audio|Datei)(?:\:\s){0,1}(?:\s\(\d\d\:\d\d\)){0,1}(.*?)\s<((?:["
+              r"a-f0-9]{8}|[a-f0-9]{16})-.*?\.[a-z0-9]{3,4})>")
 
 URL_REGEX = r"(https?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)"
 
-MESSAGE_REGEX = r"\[(\d{1,2}\.\d{1,2}\.\d{4}), (\d{1,2}:\d{2})\] (.+?): (.*)"
+GEO_REGEX = r"Ort(?:\:\s){0,1}(.*?)\s(?:<geo:([0-9\.]*),([0-9\.]*)\?.*>)"
 
-GEO_REGEX = r"(&lt;geo:([0-9\.]*),([0-9\.]*)\?.*&gt;)"
-
-CLASS_NO_MEDIA = "no_media"
-CLASS_IMAGE = "image"
-CLASS_VIDEO = "video"
-CLASS_AUDIO = "audio"
-CLASS_FILE = "file"
-CLASS_LINK = "link"
-CLASS_LOCATION = "location"
-
-parser = argparse.ArgumentParser("Threema HTML Generator")
-parser.add_argument("folder", help="The folder containing the Threema Chats.")
+parser = argparse.ArgumentParser("Threema SQLite generator")
+parser.add_argument("--user", help="The chat messages' owner.")
+parser.add_argument("--folder", help="The folder containing the Threema Chats.")
+parser.add_argument("--dbFile", help="The SQLite db file.")
 args = parser.parse_args()
+
+user = args.user.lower()
+
+if user not in ["christoph", "juliane"]:
+    raise ValueError("Invalid user")
 
 folder = args.folder
 
 if not isdir(folder):
     raise ValueError("Given value for \"folder\" is not an actual folder on this machine.")
 
+db_file = args.dbFile
+
+tmp_file = "/tmp/threema.sqlite"
+
 
 def parse_message(path, message):
-    has_image = False
-    has_video = False
-    has_audio = False
-    has_file = False
-    has_link = False
-    has_location = False
+    message_parsed = message
+    has_no_media = 1
+    image = ""
+    video = ""
+    audio = ""
+    file = ""
+    location = ""
+    has_link = 0
+    is_media_missing = 0
 
-    message = message.replace("<", "&lt;").replace(">", "&gt;")
-
-    files = re.findall(FILE_REGEX, message)
-
-    if files:
-        for match in files:
-            if not isfile(join(path, match[1])):
-                message = message.replace(match[0], f"<a href=\"{match[1]}\">MISSING FILE</a>")
-            elif match[2] in ["jpg", "jpeg", "png", "gif"]:
-                has_image = True
-                message = message.replace(match[0],
-                                          f"<br/><a href=\"{match[1]}\"><img src=\"{match[1]}\" width=300 /></a><br />")
-            elif match[2] in ["mp4"]:
-                has_video = True
-                message = message.replace(match[0],
-                                          f"<br /><a href=\"{match[1]}\"><video controls><source src=\"{match[1]}\" /></video></a><br />")
-            elif match[2] in ["aac", "opus"]:
-                has_audio = True
-                message = message.replace(match[0],
-                                          f"<br /><a href=\"{match[1]}\"><audio controls src=\"{match[1]}\"></audio></a><br />")
-            else:
-                has_file = True
-                message = message.replace(match[0], f"<a href=\"{match[1]}\">{match[1]}</a>")
+    files = re.findall(FILE_REGEX, message, re.DOTALL)
+    base = basename(path)
 
     urls = list(set(re.findall(URL_REGEX, message)))
 
-    if urls:
-        has_link = True
+    if files:
+        has_no_media = 0
 
-        for match in urls:
-            message = message.replace(match, f"<a href=\"{match}\">{match}</a>")
+        for match in files:
+            message_parsed = match[1]
+            link = f"{base}/{match[2]}".replace("'", "''")
+            if not isfile(join(path, match[2])):
+                is_media_missing = 1
+
+            if match[0] == "Bild":
+                image = link
+            elif match[0] == "Video":
+                video = link
+            elif match[0] == "Audio":
+                audio = link
+            else:
+                file = link
+
+    if urls:
+        has_link = 1
+        has_no_media = 0
 
     geos = re.findall(GEO_REGEX, message)
 
     if geos:
-        has_location = True
+        has_no_media = 0
 
-        for match in geos:
-            lat = match[1]
-            lon = match[2]
+        message_parsed = geos[0][0]
 
-            # the higher this number the further zoomed out the map
-            zoom = 0.0016
+        lat = geos[0][1]
+        lon = geos[0][2]
 
-            box_1 = str(float(lon) - zoom)
-            box_2 = str(float(lat) - zoom)
-            box_3 = str(float(lon) + zoom)
-            box_4 = str(float(lat) + zoom)
+        location = f"{lat},{lon}"
 
-            message = message.replace(match[0],
-                                      f"<iframe src=\"https://www.openstreetmap.org/export/embed.html?bbox={box_1}%2C{box_2}%2C{box_3}%2C{box_4}&amp;layer=mapnik&amp;marker={lat}%2C{lon}\"></iframe><br/><small><a href=\"https://www.openstreetmap.org/?mlat={lat}&amp;mlon={lon}#map=15/{lat}/{lon}\">View Larger Map</a></small>")
+    message_parsed = message_parsed.replace("'", "''")
 
-    message_parts = re.search(MESSAGE_REGEX, message)
-
-    if message_parts:
-        return message_parts.group(1), message_parts.group(2), message_parts.group(
-            3), message_parts.group(
-            4), has_image, has_video, has_audio, has_file, has_link, has_location
-
-    return None, None, None, message, has_image, has_video, has_audio, has_file, has_link, has_location
+    return (message_parsed, has_no_media, image, video, audio, file, location, has_link,
+            is_media_missing)
 
 
-def create_html(path):
-    message_count = 0
-    no_media_count = 0
-    image_count = 0
-    video_count = 0
-    audio_count = 0
-    file_count = 0
-    link_count = 0
-    location_count = 0
-
+def parse(path):
     messages_path = join(path, "messages.txt")
 
     if not isfile(messages_path):
         raise ValueError(f"No messages.txt in {path}")
 
-    name = basename(path)
-
     with open(messages_path, "r") as f:
-        messages = f.readlines()
+        messages = f.read()
 
-    min_date = None
-    max_date = None
+    messages_by_name = re.findall(REGEX_SPLIT_BY_NAME, messages, re.DOTALL)
 
-    content = ""
-    previous_date = None
-    previous_time = None
-    previous_align = None
+    with sqlite3.connect(tmp_file) as connection:
+        cursor = connection.cursor()
 
-    for i in range(len(messages)):
-        date, time, author, message_content, message_has_image, message_has_video, message_has_audio, message_has_file, message_has_link, message_has_location = parse_message(
-            path, messages[i])
+        chat = basename(path)
 
-        if not date:
-            # this message is just a new line of the previous message and was already processed
-            continue
+        result = cursor.execute(f"SELECT max(chat_id) FROM {user}").fetchone()[0]
 
-        classes = []
+        if result is None:
+            chat_id = 1
+        else:
+            chat_id = result + 1
 
-        j = i + 1
-        while j < len(messages):
-            # look ahead for messages belonging to this one but seperated by line breaks
-            next_date, _, _, next_message_content, next_has_image, next_has_video, next_has_audio, next_has_file, next_has_link, next_has_location = parse_message(
-                path, messages[j])
+        for message in messages_by_name:
+            message_parsed, message_has_no_media, message_image, message_video, message_audio, message_file, message_location, message_has_link, message_is_media_missing = parse_message(path, message[6])
 
-            if next_date:
-                # new message found, no further look-ahead needed
-                break
+            insert_query = f'''
+            INSERT INTO {user} (chat, chat_id, author, date, time, message, has_no_media, image, 
+            video, audio, file, location, has_link, is_media_missing) VALUES
+            ('{chat}', {chat_id}, '{message[5]}', '{message[2]}-{str(message[1].zfill(2))}-
+{str(message[0].zfill(2))}', '{message[3]}:{message[4]}', '{message_parsed}', 
+{message_has_no_media}, '{message_image}', '{message_video}', '{message_audio}', 
+'{message_file}', '{message_location}', {message_has_link}, {message_is_media_missing});
+            '''
+            cursor.execute(insert_query)
 
-            message_content += f"<br />{next_message_content}"
+        cursor.close()
+        connection.commit()
 
-            if next_has_image:
-                message_has_image = True
-
-            if next_has_video:
-                message_has_video = True
-
-            if next_has_audio:
-                message_has_audio = True
-
-            if next_has_file:
-                message_has_file = True
-
-            if next_has_link:
-                message_has_link = True
-
-            if next_has_location:
-                message_has_location = True
-
-            j += 1
-
-        message_count += 1
-
-        if message_has_image:
-            classes.append(CLASS_IMAGE)
-            image_count += 1
-
-        if message_has_video:
-            classes.append(CLASS_VIDEO)
-            video_count += 1
-
-        if message_has_audio:
-            classes.append(CLASS_AUDIO)
-            audio_count += 1
-
-        if message_has_file:
-            classes.append(CLASS_FILE)
-            file_count += 1
-
-        if message_has_link:
-            classes.append(CLASS_LINK)
-            link_count += 1
-
-        if message_has_location:
-            classes.append(CLASS_LOCATION)
-            location_count += 1
-
-        if not classes:
-            classes.append(CLASS_NO_MEDIA)
-            no_media_count += 1
-
-        align = "right" if author == "Ich" else "left"
-
-        if previous_time:
-            # this is a new message - write time of previous message
-            content += f"<br /><span class=\"time time-{previous_align}\">{previous_time}</span>"
-
-        if previous_date:
-            # we are not processing the first element
-            content += "</div></div>"
-
-        date_parts = date.split(".")
-
-        day = date_parts[0].zfill(2)
-        month = date_parts[1].zfill(2)
-        year = date_parts[2]
-
-        if date != previous_date:
-            content += f"<div class=\"date\">{day}.{month}.{year}</div>"
-            max_date = f"{year}-{month}-{day}"
-
-            if not min_date:
-                min_date = f"{year}-{month}-{day}"
-
-        content += f'<div class=\"wrapper wrapper-{align} {" ".join(classes)}\" data-date=\"{year}{month}{day}\"><div class=\"message {align}\">'
-
-        if author != "Ich":
-            content += f'<span class=\"author">{author}</span><br />'
-
-        content += f"{message_content}"
-
-        previous_date = date
-        previous_time = time
-        previous_align = align
-
-    content += f"<br /><span class=\"time time-{previous_align}\">{previous_time}</span>"
-    content += "</div></div>"
-
-    template = Path("src/index_template.html").read_text()
-
-    html = template.replace("$title", name).replace("$content", content).replace("$all",
-                                                                                 str(message_count)).replace(
-        "$no_media_count", str(no_media_count)).replace("$image_count", str(image_count)).replace(
-        "$video_count", str(video_count)).replace("$audio_count", str(audio_count)).replace(
-        "$file_count", str(file_count)).replace("$link_count", str(link_count)).replace(
-        "$location_count", str(location_count)).replace("$before_date", str(max_date)).replace(
-        "$after_date", str(min_date)).replace("$min_date", str(min_date)).replace("$max_date",
-                                                                                  str(max_date))
-
-    with open(join(path, "index.html"), "w") as f:
-        f.write(html)
-
-    shutil.copyfile("src/favicon.ico", join(path, "favicon.ico"))
+    pass
 
 
 def traverse(path):
     items = listdir(path)
 
     if "messages.txt" in items:
-        create_html(path)
+        parse(path)
         return
 
     for item in listdir(path):
@@ -274,4 +153,48 @@ def traverse(path):
             traverse(full_path)
 
 
+def init_db():
+    if isfile(tmp_file):
+        remove(tmp_file)
+
+    if not isfile(db_file):
+        mknod(tmp_file)
+
+    with sqlite3.connect(tmp_file) as connection:
+        connection.execute("PRAGMA foreign_keys = ON")
+
+        cursor = connection.cursor()
+
+        drop_table_query = f'''
+        DROP TABLE IF EXISTS {user};
+        '''
+        cursor.execute(drop_table_query)
+
+        create_table_query = f'''
+        CREATE TABLE {user} (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            chat TEXT NOT NULL,
+            chat_id INTEGER NOT NULL,
+            author TEXT NOT NULL,
+            date TEXT NOT NULL,
+            time TEXT NOT NULL,
+            message TEXT NOT NULL,
+            has_no_media INTEGER NOT NULL,
+            image string NOT NULL,
+            video string NOT NULL,
+            audio string NOT NULL,
+            file string NOT NULL,
+            location string NOT NULL,
+            has_link INTEGER NOT NULL,
+            is_media_missing INTEGER NOT NULL
+        );
+        '''
+        cursor.execute(create_table_query)
+
+        cursor.close()
+        connection.commit()
+
+
+init_db()
 traverse(folder)
+shutil.copyfile(tmp_file, db_file)
